13508a84952eda8a287b9e02ff098c71010bd794
pguyon <pguyon69@gmail.com>
2021-07-26 10:29:03 +0200

    up

--- src/DataFixtures/FilmFixtures.php
+++ src/DataFixtures/FilmFixtures.php
@@ -15,6 +15,21 @@ class FilmFixtures extends Fixture
         $film->setDescription('Un film qui fait presque peur avec presque du sang');
         $film->setRealisateur('Francis Ford Copolla');
         $manager->persist($film);
+
+        $film2 = new Film();
+        $film2->setTitre('Salut l\'ami audieu le trésor');
+        $film2->setDescription('Un film où il y a de la baffe');
+        $film2->setRealisateur('Un mec');
+        $manager->persist($film2);
+
+
+        $film3 = new Film();
+        $film3->setTitre('La Boum');
+        $film3->setDescription('Un film pour les vieux nostalgique');
+        $film3->setRealisateur('Une personne sûrement morte');
+        $manager->persist($film3);
+
+
         $manager->flush();
     }
 }
